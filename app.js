a289eeee53330ca3169cfe3a3aa9422a89fea194
Phillip Swope <ps0895423@otc.edu>
2018-09-10 09:55:13 -0500

    Finished All

--- app.js
+++ app.js
@@ -18,10 +18,21 @@ let ofMusketeers
 for (ofMusketeers of musketeers){
     console.log(ofMusketeers);
 }
-*/
+
 
 const values = [3, 11, 7, 2, 9, 10];
 let sum = 0
 
-for (i = 0; i = values.length; i++){
+for (i = 0; i < values.length; i++){
     sum+= values[i]}
+    console.log(sum)
+*/
+
+const values = [3, 11, 7, 2, 9, 10]
+let max = 0
+for (i = 0; i < values.length; i++){
+   if(values[i] > max){
+    max = values[i];
+   }
+}
+console.log(max)
